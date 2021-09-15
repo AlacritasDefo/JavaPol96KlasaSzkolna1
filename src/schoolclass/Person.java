07ec1c7d550844aab1a3db512ed652ac83d826c1
darecc <46760315+darecc@users.noreply.github.com>
2021-09-15 18:29:36 +0200

    Add files via upload

---
+++ src/schoolclass/Person.java
@@ -0,0 +1,32 @@
+package schoolclass;
+/*
+firstName (imię),
+- lastName (nazwisko),
+- getterami do pól
+- metoda toString() : imię + nazwisko
+ */
+public class Person {
+    public String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String lastName;
+    @Override
+    public String toString() {
+        return firstName + "." + lastName;
+    }
+}
